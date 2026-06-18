2b65d2c34f92bb7b02978431923fed3ef217bc7d
Kartik <kartik251020@gmail.com>
2026-06-18 22:36:48 +0530

    Update 01_data_preparation.sql

--- SQL/01_data_preparation.sql
+++ SQL/01_data_preparation.sql
@@ -1,3 +1,9 @@
+/* =========================================
+   Author: Kartik
+   Project: Customer Support Analytics
+   Portfolio: github.com/kartik-data-analytics
+   ========================================= */
+
 /*
 Customer Support Analytics Project
 
